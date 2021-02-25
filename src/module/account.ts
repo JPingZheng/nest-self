452ffc68caf275f695b40258a7fa683a0cff096c
JPing <zhengjingping@pudutech.com>
2021-02-25 20:42:45 +0800

    修改启动报错

--- src/module/account.ts
+++ src/module/account.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AccountService } from '../services/account';
 import { AccountControllers } from '../controllers/account';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from 'src/entities/account';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Account])],
   controllers: [AccountControllers],
   providers: [AccountService],
 })
